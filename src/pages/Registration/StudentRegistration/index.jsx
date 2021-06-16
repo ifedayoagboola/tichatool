b8bf586abab25d532cca2215715f4cb472922b01
ifedayo <ifedayoagboola19@gmail.com>
2021-06-16 16:56:25 +0100

    latest commit

--- src/pages/Registration/StudentRegistration/index.jsx
+++ src/pages/Registration/StudentRegistration/index.jsx
@@ -4,8 +4,6 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
@@ -58,7 +56,7 @@ export default function SignUp() {
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Next
+          Sign Up
         </Typography>
         <form className={classes.form} noValidate>
           <Grid container spacing={3}>
